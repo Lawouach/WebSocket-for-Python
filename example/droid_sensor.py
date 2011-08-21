090174df47d10140cede2410d272e9e003efbb93
Sylvain Hellegouarch <sh@defuze.org>
2011-08-21 22:36:20 +0200

    adding some example: a chat simple demo and an android based sensor dispatcher

---
+++ example/droid_sensor.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+__doc__ = """
+WebSocket client that pushes Android sensor metrics to the
+websocket server it is connected to.
+
+In order to set this up:
+
+ 1. Install SL4A http://code.google.com/p/android-scripting/
+ 2. Install the Python package for SL4A
+ 3. Build ws4py and copy the package built into a directory
+    called com.googlecode.pythonforandroid/extras/python on
+    your Android device.
+ 4. a. Either copy the droid_sensor.py module into the directory
+       called sl4a/scripts on your Android device.
+    b. Or set up the remote control so that you can run the
+       module from your computer directly on your device:
+       http://code.google.com/p/android-scripting/wiki/RemoteControl
+ 5. Setup the device so that it has an IP address on the same
+    network as the computer running the server.
+    
+Run the example:
+
+ 1. Start the echo_cherrypy_server module:
+    $ python example/echo_cherrypy_server.py
+ 2. From a local browser, go to:
+    http://localhost:9000/
+ 3. Edit the droid_sensor module to set the appropriate
+    IP address where your server is running.
+ 4. Run the droid_sensor module (from the device or
+    your computer depending on your setup):
+    $ python example/droid_sensor.py
+ 5. If your device isn't idled, just move ir around
+    for the metrics to be sent to the server which
+    will dispatch them to the browser's client.
+ 6. Profit???
+ 
+"""
+import time
+import math
+
+import android
+from ws4py.client.threadedclient import WebSocketClient
+
+class AirPongSensor(object):
+    def __init__(self, host):
+        self.droid = android.Android()
+        #self.droid.startSensingThreshold(1, 0, 7)
+        #self.droid.startSensingThreshold(2, 1, 2)
+        self.droid.startSensingTimed(1, 500)
+
+        self.running = False
+
+        self.client = AirPongWebSocketClient(host)
+        self.client.connect()
+        
+    def run(self):
+        try:
+            self.running = True
+            last = [None, None, None]
+            while self.running:
+                azimuth, pitch, roll = self.droid.sensorsReadOrientation().result
+                accel = x, y, z = self.droid.sensorsReadAccelerometer().result
+                if azimuth is None:
+                    continue
+                
+                c = lambda rad: rad * 360.0 / math.pi
+                print c(azimuth), c(pitch), c(roll), x, y, z
+
+                if self.client.terminated:
+                    break
+
+                if accel != [None, None, None] and accel != last:
+                    last = accel
+                    self.client.send("%s %s %s %s %s %s" % (c(azimuth), c(pitch), c(roll), x, y, z))
+
+                time.sleep(1)
+        finally:
+            self.terminate()
+            
+    def terminate(self):
+        if not self.droid:
+            return
+        
+        self.running = False
+        self.droid.stopSensing()
+        self.droid = None
+        
+        if not self.client.terminated:
+            self.client.close()
+            self.client._th.join()
+            self.client = None
+        
+class AirPongWebSocketClient(WebSocketClient):
+        def received_message(self, m):
+            print m, len(str(m))
+
+if __name__ == '__main__':
+    aps = AirPongSensor(host='http://192.168.0.10:9000/ws')
+    try:
+        aps.run()
+    except KeyboardInterrupt:
+        aps.terminate()
---
+++ example/echo_cherrypy_server.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import random
+
+import cherrypy
+
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.server.handler.threadedhandler import WebSocketHandler, EchoWebSocketHandler
+
+class ChatWebSocketHandler(WebSocketHandler):
+    def received_message(self, m):
+        cherrypy.engine.publish('websocket-broadcast', str(m))
+        
+class Root(object):
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+    <head>
+      <script type='application/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'> </script>
+      <script type='application/javascript'>
+        $(document).ready(function() {
+          var ws = new WebSocket('ws://localhost:9000/ws');
+          ws.onmessage = function (evt) {
+             $('#chat').val($('#chat').val() + evt.data + '\\n');                  
+          };
+          ws.onopen = function() {
+             ws.send("%(username)s entered the room");
+          };
+          $('#chatform').submit(function() {
+             ws.send('%(username)s: ' + $('#message').val());
+             $('#message').val("");
+             return false;
+          });
+        });
+      </script>
+    </head>
+    <body>
+    <form action='/echo' id='chatform' method='get'>
+      <textarea id='chat' cols='35' rows='10'></textarea>
+      <br />
+      <label for='message'>%(username)s: </label><input type='text' id='message' />
+      <input type='submit' value='Send' />
+      </form>
+    </body>
+    </html>
+    """ % {'username': "User%d" % random.randint(0, 100)}
+
+    @cherrypy.expose
+    def ws(self):
+        cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
+
+if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                            'server.socket_port': 9000})
+    WebSocketPlugin(cherrypy.engine).subscribe()
+    cherrypy.tools.websocket = WebSocketTool()
+
+    cherrypy.quickstart(Root(), '', config={
+        '/ws': {
+            'tools.websocket.on': True,
+            'tools.websocket.handler_cls': ChatWebSocketHandler
+            }
+        }
+    )
