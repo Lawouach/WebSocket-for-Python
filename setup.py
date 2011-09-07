dc200a02323c6e63d99a8847c4812e6297178b76
Sylvain Hellegouarch <sh@defuze.org>
2011-09-07 21:39:57 +0200

    added classifiers

--- setup.py
+++ setup.py
@@ -15,5 +15,17 @@ setup(name = "ws4py",
                   "ws4py.server.handler", "ws4py.server.wsgi"],
       platforms = ["any"],
       license = 'BSD',
-      long_description = "WebSocket library for the Python"
+      long_description = "WebSocket library for the Python",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Framework :: CherryPy',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent'
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Communications',
+          'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
      )
