17810cdc1880b8ff7d59391e1f28261c7aabb2aa
Sylvain Hellegouarch <sh@defuze.org>
2012-04-10 22:56:22 +0200

    masking is required, fixed a couple of minor corner cases

--- test/test_stream.py
+++ test/test_stream.py
@@ -10,25 +10,24 @@ from ws4py.messaging import CloseControlMessage
 
 class WSStreamTest(unittest.TestCase):
     def test_empty_close_message(self):
-        f = Frame(opcode=OPCODE_CLOSE, body='', fin=1).build()
+        f = Frame(opcode=OPCODE_CLOSE, body='', fin=1, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(s.closing, None)
         s.parser.send(f)
         self.assertEqual(type(s.closing), CloseControlMessage)
     
     def test_too_large_close_message(self):
-        f = Frame(opcode=OPCODE_CLOSE, body='*' * 330, fin=1).build()
+        f = Frame(opcode=OPCODE_CLOSE, body='*' * 330, fin=1, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(s.closing, None)
         s.parser.send(f)
-        self.assertEqual(s.closing, None)
-        self.assertEqual(len(s.errors), 1)
-        self.assertEqual(type(s.errors[0]), CloseControlMessage)
-        self.assertEqual(s.errors[0].code, 1002)
+        
+        self.assertEqual(type(s.closing), CloseControlMessage)
+        self.assertEqual(s.closing.code, 1002)
     
     def test_ping_message_received(self):
         msg = 'ping me'
-        f = Frame(opcode=OPCODE_PING, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_PING, body=msg, fin=1, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(len(s.pings), 0)
         s.parser.send(f)
@@ -37,7 +36,7 @@ class WSStreamTest(unittest.TestCase):
         
     def test_text_message_received(self):
         msg = 'hello there'
-        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=1, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(len(s.messages), 0)
         s.parser.send(f)
@@ -45,7 +44,7 @@ class WSStreamTest(unittest.TestCase):
     
     def test_incremental_text_message_received(self):
         msg = 'hello there'
-        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=1, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(s.has_message, False)
         for byte in f:
@@ -54,7 +53,7 @@ class WSStreamTest(unittest.TestCase):
     
     def test_text_message_received(self):
         msg = 'hello there'
-        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=1, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(s.has_message, False)
         s.parser.send(f)
@@ -62,20 +61,20 @@ class WSStreamTest(unittest.TestCase):
 
     def test_text_message_with_continuation_received(self):
         msg = 'hello there'
-        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=0).build()
+        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=0, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(s.has_message, False)
         s.parser.send(f)
         self.assertEqual(s.message.completed, False)
 
         for i in range(3):
-            f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=0).build()
+            f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=0, masking_key=os.urandom(4)).build()
             s.parser.send(f)
             self.assertEqual(s.has_message, False)
             self.assertEqual(s.message.completed, False)
             self.assertEqual(s.message.opcode, OPCODE_TEXT)
         
-        f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=1, masking_key=os.urandom(4)).build()
         s.parser.send(f)
         self.assertEqual(s.has_message, True)
         self.assertEqual(s.message.completed, True)
@@ -83,25 +82,26 @@ class WSStreamTest(unittest.TestCase):
         
     def test_text_message_with_continuation_and_ping_in_between(self):
         msg = 'hello there'
-        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=0).build()
+        key = os.urandom(4)
+        f = Frame(opcode=OPCODE_TEXT, body=msg, fin=0, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(s.has_message, False)
         s.parser.send(f)
         self.assertEqual(s.message.completed, False)
 
         for i in range(3):
-            f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=0).build()
+            f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=0, masking_key=os.urandom(4)).build()
             s.parser.send(f)
             self.assertEqual(s.has_message, False)
             self.assertEqual(s.message.completed, False)
             self.assertEqual(s.message.opcode, OPCODE_TEXT)
         
-            f = Frame(opcode=OPCODE_PING, body='ping me', fin=1).build()
+            f = Frame(opcode=OPCODE_PING, body='ping me', fin=1, masking_key=os.urandom(4)).build()
             self.assertEqual(len(s.pings), i)
             s.parser.send(f)
             self.assertEqual(len(s.pings), i+1)
         
-        f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=1, masking_key=os.urandom(4)).build()
         s.parser.send(f)
         self.assertEqual(s.has_message, True)
         self.assertEqual(s.message.opcode, OPCODE_TEXT)
@@ -109,7 +109,7 @@ class WSStreamTest(unittest.TestCase):
         
     def test_binary_message_received(self):
         msg = os.urandom(16)
-        f = Frame(opcode=OPCODE_BINARY, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_BINARY, body=msg, fin=1, masking_key=os.urandom(4)).build()
         s = Stream()
         self.assertEqual(s.has_message, False)
         s.parser.send(f)
@@ -117,20 +117,21 @@ class WSStreamTest(unittest.TestCase):
 
     def test_binary_message_with_continuation_received(self):
         msg = os.urandom(16)
-        f = Frame(opcode=OPCODE_BINARY, body=msg, fin=0).build()
+        key = os.urandom(4)
+        f = Frame(opcode=OPCODE_BINARY, body=msg, fin=0, masking_key=key).build()
         s = Stream()
         self.assertEqual(s.has_message, False)
         s.parser.send(f)
-        self.assertEqual(s.message.completed, False)
+        self.assertEqual(s.has_message, False)
 
         for i in range(3):
-            f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=0).build()
+            f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=0, masking_key=key).build()
             s.parser.send(f)
             self.assertEqual(s.has_message, False)
             self.assertEqual(s.message.completed, False)
             self.assertEqual(s.message.opcode, OPCODE_BINARY)
         
-        f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=1).build()
+        f = Frame(opcode=OPCODE_CONTINUATION, body=msg, fin=1, masking_key=key).build()
         s.parser.send(f)
         self.assertEqual(s.has_message, True)
         self.assertEqual(s.message.completed, True)
