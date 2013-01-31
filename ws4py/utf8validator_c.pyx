88b34063a05c5757dfa643606dca0f47e31187f9
INADA Naoki <inada-n@klab.com>
2013-01-31 09:21:37 +0900

    more fast cython code

--- ws4py/utf8validator_c.pyx
+++ ws4py/utf8validator_c.pyx
@@ -29,9 +29,17 @@
 ##
 ###############################################################################
 
-from ws4py.compat import range
+## DFA transitions
+cdef extern from "dfa.h":
+    cdef char* UTF8VALIDATOR_DFA
 
-class Utf8Validator(object):
+cdef extern from "Python.h":
+    int PyObject_AsReadBuffer(object, void**, Py_ssize_t*) except -1
+
+DEF _UTF8_ACCEPT = 0
+DEF _UTF8_REJECT = 1
+
+cdef class Utf8Validator(object):
     """
     Incremental UTF-8 validator with constant memory consumption (minimal state).
 
@@ -39,26 +47,12 @@ class Utf8Validator(object):
     Bjoern Hoehrmann (http://bjoern.hoehrmann.de/utf-8/decoder/dfa/).
     """
 
-    ## DFA transitions
-    UTF8VALIDATOR_DFA = [
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, # 00..1f
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, # 20..3f
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, # 40..5f
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, # 60..7f
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9, # 80..9f
-        7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7, # a0..bf
-        8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, # c0..df
-        0xa,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x4,0x3,0x3, # e0..ef
-        0xb,0x6,0x6,0x6,0x5,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8, # f0..ff
-        0x0,0x1,0x2,0x3,0x5,0x8,0x7,0x1,0x1,0x1,0x4,0x6,0x1,0x1,0x1,0x1, # s0..s0
-        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1, # s1..s2
-        1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1, # s3..s4
-        1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1, # s5..s6
-        1,3,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1, # s7..s8
-    ]
-
     UTF8_ACCEPT = 0
     UTF8_REJECT = 1
+
+    cdef public int i
+    cdef public int state
+    cdef public int codepoint
 
     def __init__(self):
         self.reset()
@@ -74,19 +68,19 @@ class Utf8Validator(object):
 
         Returns some other positive integer when more octets need to be eaten.
         """
-        type = Utf8Validator.UTF8VALIDATOR_DFA[b]
-        if self.state != Utf8Validator.UTF8_ACCEPT:
+        cdef int type = UTF8VALIDATOR_DFA[b]
+        if self.state != _UTF8_ACCEPT:
             self.codepoint = (b & 0x3f) | (self.codepoint << 6)
         else:
             self.codepoint = (0xff >> type) & b
-        self.state = Utf8Validator.UTF8VALIDATOR_DFA[256 + self.state * 16 + type]
+        self.state = UTF8VALIDATOR_DFA[256 + self.state * 16 + type]
         return self.state
 
     def reset(self):
         """
         Reset validator to start new incremental UTF-8 decode/validation.
         """
-        self.state = Utf8Validator.UTF8_ACCEPT
+        self.state = _UTF8_ACCEPT
         self.codepoint = 0
         self.i = 0
 
@@ -103,15 +97,19 @@ class Utf8Validator(object):
         When valid? == True, currentIndex will be len(ba) and totalIndex the
         total amount of consumed bytes.
         """
-        state = self.state
-        DFA = Utf8Validator.UTF8VALIDATOR_DFA
-        for i, b in enumerate(ba):
+        cdef int state = self.state
+        cdef int i, b
+        cdef char* buf
+        cdef Py_ssize_t buf_len
+        PyObject_AsReadBuffer(ba, <void**>&buf, &buf_len)
+        for i in range(buf_len):
+            b = buf[i]
             ## optimized version of decode(), since we are not interested in actual code points
-            state = DFA[256 + (state << 4) + DFA[b]]
-            if state == Utf8Validator.UTF8_REJECT:
+            state = UTF8VALIDATOR_DFA[256 + (state << 4) + UTF8VALIDATOR_DFA[b]]
+            if state == _UTF8_REJECT:
                 self.i += i
                 self.state = state
                 return False, False, i, self.i
         self.i += i
         self.state = state
-        return True, state == Utf8Validator.UTF8_ACCEPT, i, self.i
+        return True, state == _UTF8_ACCEPT, i, self.i
