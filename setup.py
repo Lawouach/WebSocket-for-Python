2d21b8632a76f54d52db3c17daa3f48e376924e3
Sylvain Hellegouarch <sh@defuze.org>
2013-02-02 12:16:49 +0100

    branching for 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name = "ws4py",
-      version = '0.2.5',
+      version = '0.3.0',
       description = "WebSocket library for Python",
       maintainer = "Sylvain Hellegouarch",
       maintainer_email = "sh@defuze.org",
