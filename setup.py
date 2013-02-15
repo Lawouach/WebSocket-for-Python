ccc600a96053c51903a3335524efc943e1f3227a
INADA Naoki <inada-n@klab.com>
2013-02-16 03:03:43 +0900

    cython: fallback to pure python utf8validator

--- setup.py
+++ setup.py
@@ -1,11 +1,74 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
 try:
     from setuptools import setup, Extension
 except ImportError:
     from distutils.core import setup, Extension
 
-_c_utf8validator = Extension('ws4py.utf8validator_c', ['ws4py/utf8validator_c.c'])
+from distutils.command.build_ext import build_ext
+from distutils.command.sdist import sdist
+
+try:
+    import Cython.Compiler.Main as cython_compiler
+    have_cython = True
+except ImportError:
+    have_cython = False
+
+
+class NoCython(Exception):
+    pass
+
+def cythonize(src):
+    sys.stderr.write("cythonize: %r\n" % (src,))
+    cython_compiler.compile([src])
+
+def ensure_source(src):
+    pyx = os.path.splitext(src)[0] + '.pyx'
+
+    if not os.path.exists(src):
+        if not have_cython:
+            raise NoCython
+        cythonize(pyx)
+    elif (os.path.exists(pyx) and
+          os.stat(src).st_mtime < os.stat(pyx).st_mtime and
+          have_cython):
+        cythonize(pyx)
+    return src
+
+
+class BuildExt(build_ext):
+    def build_extension(self, ext):
+        try:
+            ext.sources = list(map(ensure_source, ext.sources))
+        except NoCython:
+            print("WARNING")
+            print("Cython is required for building extension from checkout.")
+            print("Install Cython >= 0.16 or install ws4py from PyPI.")
+            print("Falling back to pure Python implementation.")
+            return
+        try:
+            return build_ext.build_extension(self, ext)
+        except Exception as e:
+            print("WARNING: Failed to compile extensiom modules.")
+            print("ws4py uses fallback pure python implementation.")
+            print(e)
+
+
+# take care of extension modules.
+if have_cython:
+    class Sdist(sdist):
+        def __init__(self, *args, **kwargs):
+            from glob import glob
+            for src in glob('ws4py/*.pyx'):
+                cythonize(src)
+            sdist.__init__(self, *args, **kwargs)
+else:
+    Sdist = sdist
+
+ext_modules = [
+    Extension('ws4py.utf8validator_c', ['ws4py/utf8validator_c.c']),
+    ]
 
 test_require = ['unittest2', 'cherrypy']
 if sys.version_info < (3, 0):
@@ -19,7 +82,8 @@ setup(name = "ws4py",
       url = "https://github.com/Lawouach/WebSocket-for-Python",
       download_url = "http://www.defuze.org/oss/ws4py/",
       packages = ["ws4py", "ws4py.client", "ws4py.server"],
-      ext_modules = [_c_utf8validator],
+      cmdclass={'build_ext': BuildExt, 'sdist': Sdist},
+      ext_modules=ext_modules,
       tests_require=test_require,
       test_suite='unittest2.collector',
       platforms = ["any"],
