7f2853849333e04193ff8da81a3bf2f6752803af
Sylvain Hellegouarch <sh@defuze.org>
2013-08-08 19:47:21 +0200

    a little more tests

--- test/test_frame.py
+++ test/test_frame.py
@@ -7,6 +7,7 @@ import random
 from ws4py.framing import Frame, \
      OPCODE_CONTINUATION, OPCODE_TEXT, \
      OPCODE_BINARY, OPCODE_CLOSE, OPCODE_PING, OPCODE_PONG
+from ws4py.exc import FrameTooLargeException, ProtocolException
 from ws4py.compat import *
 
 def map_on_bytes(f, bytes):
@@ -18,7 +19,7 @@ class WSFrameBuilderTest(unittest.TestCase):
         f = Frame(opcode=OPCODE_TEXT,
                   body=enc(''), fin=1)
         self.assertEqual(len(f.build()), 2)
-        
+
         f = Frame(opcode=OPCODE_TEXT,
                   body=enc('*' * 125), fin=1)
         self.assertEqual(len(f.build()), 127)
@@ -40,7 +41,7 @@ class WSFrameBuilderTest(unittest.TestCase):
         f = Frame(opcode=OPCODE_TEXT,
                   body=enc('*' * 65535), fin=1)
         self.assertEqual(len(f.build()), 65539)
-        
+
         mask = os.urandom(4)
         f = Frame(opcode=OPCODE_TEXT,
                   body=enc('*' * 126), masking_key=mask, fin=1)
@@ -49,7 +50,7 @@ class WSFrameBuilderTest(unittest.TestCase):
         f = Frame(opcode=OPCODE_TEXT,
                   body='*' * 65535, masking_key=mask, fin=1)
         self.assertEqual(len(f.build()), 65543)
-        
+
     def test_63_bit_length(self):
         f = Frame(opcode=OPCODE_TEXT,
                   body=enc('*' * 65536), fin=1)
@@ -87,6 +88,12 @@ class WSFrameBuilderTest(unittest.TestCase):
         else: spec_says = '\x81\x857\xfa!=\x7f\x9fMQX'
         self.assertEqual(f.build(), spec_says)
 
+    def test_frame_too_large(self):
+        f = Frame(opcode=OPCODE_TEXT, body=enc(''), fin=1)
+        # fake huge length
+        f.payload_length = 1 << 63
+        self.assertRaises(FrameTooLargeException, f.build)
+
 class WSFrameParserTest(unittest.TestCase):
     def test_frame_parser_is_a_generator(self):
         f = Frame()
@@ -107,7 +114,7 @@ class WSFrameParserTest(unittest.TestCase):
         self.assertTrue(f.masking_key is None)
         self.assertEqual(f.payload_length, 5)
         f.parser.close()
-        
+
     def test_frame_payload_parsing(self):
         bytes = Frame(opcode=OPCODE_TEXT, body='hello', fin=1).build()
 
@@ -116,15 +123,15 @@ class WSFrameParserTest(unittest.TestCase):
         self.assertEqual(f.parser.send(bytes[1:2]), 5)
         f.parser.send(bytes[2:])
         self.assertEqual(f.body, enc('hello'))
-        
+
         f = Frame()
         f.parser.send(bytes)
         self.assertRaises(StopIteration, next, f.parser)
         self.assertEqual(f.body, enc('hello'))
-        
+
     def test_incremental_parsing_small_7_bit_length(self):
         bytes = Frame(opcode=OPCODE_TEXT, body=enc('hello'), fin=1).build()
-        
+
         f = Frame()
         map_on_bytes(f.parser.send, bytes)
         self.assertTrue(f.masking_key is None)
@@ -132,7 +139,7 @@ class WSFrameParserTest(unittest.TestCase):
 
     def test_incremental_parsing_16_bit_length(self):
         bytes = Frame(opcode=OPCODE_TEXT, body=enc('*' * 126), fin=1).build()
-        
+
         f = Frame()
         map_on_bytes(f.parser.send, bytes)
         self.assertTrue(f.masking_key is None)
@@ -140,11 +147,43 @@ class WSFrameParserTest(unittest.TestCase):
 
     def test_incremental_parsing_63_bit_length(self):
         bytes = Frame(opcode=OPCODE_TEXT, body=enc('*' * 65536), fin=1).build()
-        
+
         f = Frame()
         map_on_bytes(f.parser.send, bytes)
         self.assertTrue(f.masking_key is None)
         self.assertEqual(f.payload_length, 65536)
+
+    def test_rsv1_bits_set(self):
+        f = Frame()
+        self.assertRaises(ProtocolException, f.parser.send, enc('\x40'))
+
+    def test_rsv2_bits_set(self):
+        f = Frame()
+        self.assertRaises(ProtocolException, f.parser.send, enc('\x20'))
+
+    def test_rsv3_bits_set(self):
+        f = Frame()
+        self.assertRaises(ProtocolException, f.parser.send, enc('\x10'))
+
+    def test_invalid_opcode(self):
+        for opcode in range(3, 9):
+            f = Frame()
+            self.assertRaises(ProtocolException, f.parser.send, enc(chr(opcode)))
+
+        f = Frame()
+        self.assertRaises(ProtocolException, f.parser.send, enc(chr(10)))
+
+    def test_fragmented_control_frame_is_invalid(self):
+        f = Frame()
+        self.assertRaises(ProtocolException, f.parser.send, enc('0x9'))
+
+    def test_frame_sized_below_127(self):
+        bytes = Frame(opcode=OPCODE_TEXT, body=enc('*'*65536), fin=1).build()
+
+        f = Frame()
+        f.parser.send(bytes[:3])
+        self.assertTrue(f.masking_key is None)
+        self.assertEqual(f.payload_length, 127)
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
