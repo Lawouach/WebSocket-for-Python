bf46d371f88a301408291aa0f3b94e9f206fed81
Sylvain Hellegouarch <sh@defuze.org>
2013-09-12 19:59:10 +0200

    Believe it's good to go

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name = "ws4py",
-      version = '0.3.2-beta',
+      version = '0.3.2',
       description = "WebSocket library for Python",
       maintainer = "Sylvain Hellegouarch",
       maintainer_email = "sh@defuze.org",
