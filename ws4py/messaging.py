123bed4d5106c17c20ae6e55a95e68261ac97965
Sylvain Hellegouarch <sh@defuze.org>
2014-01-19 21:10:26 +0100

    Fix #117

--- ws4py/messaging.py
+++ ws4py/messaging.py
@@ -104,6 +104,8 @@ class Message(object):
         return len(self.__unicode__())
 
     def __str__(self):
+        if py3k:
+            return self.data.decode(self.encoding)
         return self.data
 
     def __unicode__(self):
@@ -151,6 +153,8 @@ class CloseControlMessage(Message):
         self.reason = reason
 
     def __str__(self):
+        if py3k:
+            return self.reason.decode('utf-8')
         return self.reason
 
     def __unicode__(self):
