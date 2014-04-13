47b218d73528e89add2cc3d43295e0f6bf42bf6e
Sylvain Hellegouarch <sh@defuze.org>
2014-04-13 15:01:46 +0200

    added support for device motion events. Also minified resources

--- example/websensors/static/js/drawingboard.js
+++ example/websensors/static/js/drawingboard.js
@@ -14,6 +14,9 @@
 		      '#99dd00', '#22aaff', '#ff22aa'];
 	var color = "#ff1100";
 	
+	var pos = {x: 0, y: 0};
+	var accel = {x: 0, y: 0};
+
 	me.connect = function() {
 	    ws = new WebSocket(options.url+'/ws/?board_id='+board_id+"&participant_id="+participant_id);
 	    ws.onopen = function(e) {};
@@ -21,8 +24,8 @@
 		alert("Connection lost: " + e.reason);
 	    };
 	    ws.onmessage = function(e) {
-		console.log(e);
 		if (e.data == 'clear') {
+		    me.stop_motion();
 		    board.clearCanvas();
 		} else {
 		    var state = $.parseJSON(e.data);
@@ -80,12 +83,12 @@
 
 	    tools.drawText({
 		layer: true,
-		fillStyle: '#fff',
-		x: 30, y: 20,
+		fillStyle: '#fff',		x: 30, y: 20,
 		fontSize: 14,
 		fontFamily: 'Verdana, sans-serif',
 		text: 'Clear',
 		click: function(layer) {
+		    me.stop_motion();
 		    board.clearCanvas();
 		    me.send_clear_board();
 		}
@@ -110,6 +113,52 @@
 	    }
 	};
 
+	me.motion = function(e) {
+	    var target = $(event.target);
+	    if (e.type == "devicemotion") {
+		accel.x = accel.x * 0.9 + e.accelerationIncludingGravity.x * 1.5;
+		accel.y = accel.y * 0.9 + e.accelerationIncludingGravity.z * 1.5;
+		
+		if ((pos.x < 0) || (pos.x > arena.width())) {
+		    accel.x = -accel.x;
+		}
+		if ((pos.y < 0) || (pos.y > arena.height())) {
+		    accel.y = -accel.y;
+		}
+		
+		pos.x = pos.x + accel.x;
+		pos.y = pos.y - accel.y;
+	    } else if (e.type == "mousemove") {
+		var offset = target.offset();
+		pos.x = e.clientX - offset.left;
+		pos.y = e.clientY - offset.top;
+	    } else if (e.type == "mousedown") {
+		var offset = target.offset();
+		pos.x = e.clientX - offset.left;
+		pos.y = e.clientY - offset.top;
+	    }
+	    
+	    if ((pos.x > 0) && (pos.y > 0)) {
+		me.send_state(pos.x, pos.y, color);
+		me.draw(pos.x, pos.y);
+	    }
+
+	    e.preventDefault();
+	    e.stopPropagation();
+	};
+
+	me.stop_motion = function() {
+	    if (me.ismobile()) {
+		window.removeEventListener('devicemotion', me.motion, true);
+	    }
+	}
+
+	me.ismobile = function() {
+	    // cheap way to decide if we're mobile
+	    // not bullet proof but this is a demo
+	    return (Modernizr.touch == true) && window.DeviceMotionEvent;
+	};
+	
 	me.init = function() {
 	  me.connect();
 
@@ -117,28 +166,30 @@
 	  board = $("#board");
 	  tools = $("#tools");
 	  me.resize();
-
           $(window).resize(me.resize);
 	    
 	  me.setup();
 
 	  board.mousedown(function(e) {
-	      var offset = $(this).offset();
-	      var x = e.clientX - offset.left;
-	      var y = e.clientY - offset.top;
-	      me.draw(x, y);
-	      me.send_state(x, y, color);
-	      board.mousemove(function(e) {
-		  var offset = $(this).offset();
-		  var x = e.clientX - offset.left;
-		  var y = e.clientY - offset.top;
-		  me.draw(x, y);
-		  me.send_state(x, y, color);
-	      });
+	      me.motion(e);
+
+	      if (me.ismobile()) {
+		  me.stop_motion();  // just in case
+
+		  // this is a bitch. Neither 'mouseup' nor 'touchend'
+		  // are dispatched when the user release his finger
+		  // from pressing on the screen. So, once started, this
+		  // event will continue. sigh.
+		  // Press on the "clear" button to stop that event.
+		  window.addEventListener('devicemotion', me.motion, true);
+	      } else {
+		  board.mousemove(me.motion);
+              }
 	  });
 
 	  board.mouseup(function(e) {
 	      board.unbind('mousemove');
+	      me.stop_motion();
           });
 	};
 
