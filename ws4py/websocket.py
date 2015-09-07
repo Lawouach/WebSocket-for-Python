0a02890645f9b1421ad558d0a9db774d51830713
Daniel Dunn <eternityforest@gmail.com>
2015-09-07 01:31:54 -0700

    Add fix for ssl thread issue by wrapping things in lock.
    Conflicts:
    	ws4py/websocket.py

--- ws4py/websocket.py
+++ ws4py/websocket.py
@@ -130,6 +130,11 @@ class WebSocket(object):
 
         self._local_address = None
         self._peer_address = None
+        
+        self.lock = threading.Lock()
+    
+        "Used to signal that the server side should be shutting down"
+        self.server_terminate_request = False
 
     @property
     def local_address(self):
@@ -174,9 +179,14 @@ class WebSocket(object):
 
         .. seealso:: Defined Status Codes http://tools.ietf.org/html/rfc6455#section-7.4.1
         """
-        if not self.server_terminated:
-            self.server_terminated = True
-            self._write(self.stream.close(code=code, reason=reason).single(mask=self.stream.always_mask))
+        
+        #If we are sending a fragmented frame with a generator this will make that stop
+        self.server_terminate_request = True
+        
+        with self.lock:
+            if not self.server_terminated:
+                self.server_terminated = True
+                self._write(self.stream.close(code=code, reason=reason).single(mask=self.stream.always_mask))
 
     def closed(self, code, reason=None):
         """
@@ -284,21 +294,27 @@ class WebSocket(object):
 
         if isinstance(payload, basestring) or isinstance(payload, bytearray):
             m = message_sender(payload).single(mask=self.stream.always_mask)
-            self._write(m)
+            with self.lock:
+                self._write(m)
 
         elif isinstance(payload, Message):
             data = payload.single(mask=self.stream.always_mask)
-            self._write(data)
+            with self.lock:
+                self._write(data)
 
         elif type(payload) == types.GeneratorType:
-            bytes = next(payload)
-            first = True
-            for chunk in payload:
-                self._write(message_sender(bytes).fragment(first=first, mask=self.stream.always_mask))
-                bytes = chunk
-                first = False
+            with self.lock:
+                bytes = next(payload)
+                first = True
+                for chunk in payload:
+                    #This lets close() interrupt even a long running send.
+                    if self.server_terminate_request:
+                        break
+                    self._write(message_sender(bytes).fragment(first=first, mask=self.stream.always_mask))
+                    bytes = chunk
+                    first = False
 
-            self._write(message_sender(bytes).fragment(last=True, mask=self.stream.always_mask))
+                self._write(message_sender(bytes).fragment(last=True, mask=self.stream.always_mask))
 
         else:
             raise ValueError("Unsupported type '%s' passed to send()" % type(payload))
