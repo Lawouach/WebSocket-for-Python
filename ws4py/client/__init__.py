18b8fcfeedcca78d9406cc5c43d44e2076df5bb3
William Grzybowski <wg@FreeBSD.org>
2018-03-16 10:29:01 -0300

    Remove SO_REUSEADDR
    Reusing address for ephemeral ports is not a good idea and may cause
    "Address already in use" error.
    See https://gavv.github.io/blog/ephemeral-port-reuse/ for a more
    detailed explanation.

--- ws4py/client/__init__.py
+++ ws4py/client/__init__.py
@@ -110,7 +110,6 @@ class WebSocketBaseClient(WebSocket):
 
             sock = socket.socket(family, socktype, proto)
             sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             if hasattr(socket, 'AF_INET6') and family == socket.AF_INET6 and \
               self.host.startswith('::'):
                 try:
