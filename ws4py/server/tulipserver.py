dfd2edf235cec2f632876e769a9142eba5a25a86
Fabien Boucher <fboucher@redhat.com>
2020-03-12 10:34:23 +0000

    Python 3.7 and 3.8 compat

--- ws4py/server/tulipserver.py
+++ ws4py/server/tulipserver.py
@@ -40,7 +40,7 @@ class WebSocketProtocol(asyncio.StreamReaderProtocol):
         #self.stream.set_transport(transport)
         asyncio.StreamReaderProtocol.connection_made(self, transport)
         # Let make it concurrent for others to tag along
-        f = asyncio.async(self.handle_initial_handshake())
+        f = asyncio.ensure_future(self.handle_initial_handshake())
         f.add_done_callback(self.terminated)
 
     @property
