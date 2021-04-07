29cf973a2b86ecd1156fcf0185148f40a77261b5
liuchong <mail@liuchong.io>
2021-04-07 16:02:06 +0800

    Fix passing host to in headers

--- ws4py/client/__init__.py
+++ ws4py/client/__init__.py
@@ -253,7 +253,6 @@ class WebSocketBaseClient(WebSocket):
         handshake.
         """
         headers = [
-            ('Host', '%s:%s' % (self.host, self.port)),
             ('Connection', 'Upgrade'),
             ('Upgrade', 'websocket'),
             ('Sec-WebSocket-Key', self.key.decode('utf-8')),
@@ -265,6 +264,10 @@ class WebSocketBaseClient(WebSocket):
 
         if self.extra_headers:
             headers.extend(self.extra_headers)
+
+        if not any(x for x in headers if x[0].lower() == 'host') and \
+           'host' not in self.exclude_headers:
+            headers.append(('Host', '%s:%s' % (self.host, self.port)))
 
         if not any(x for x in headers if x[0].lower() == 'origin') and \
            'origin' not in self.exclude_headers:
