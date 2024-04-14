646dbe9deaf29a8c296126d75fc157d7e1c2050c
Alexandre Detiste <alexandre.detiste@gmail.com>
2024-04-14 17:01:38 +0200

    prefer newer unittest.mock when available

--- test/test_client.py
+++ test/test_client.py
@@ -5,7 +5,10 @@ import socket
 import time
 import unittest
 
-from mock import MagicMock, patch
+try:
+    from unittest.mock import MagicMock, patch
+except ImportError:
+    from mock import MagicMock, patch
 
 from ws4py import WS_KEY
 from ws4py.exc import HandshakeError
